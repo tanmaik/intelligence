6de1cb6e70db3734428a5144f1e70bab8a554d9a
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-12-27 14:13:31 -0500

    solely use bun

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ app.get("/", (c) => c.text("keep a pulse"));
 
 app.notFound((c) => c.text("huh", 404));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 if (import.meta.main) {
   serve({
