c7c2bceaacb64ebf31588c8088e2b35bc1736836
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2024-12-27 23:34:03 -0500

    undo client breakage

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,3 +1,6 @@
-export default function Home() {
-  return <div>Hello World</div>;
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default async function Home() {
+  return <div className="p-4 flex justify-center max-w-xl"></div>;
 }
