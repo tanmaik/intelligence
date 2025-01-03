4c38479ca26171c3567be1ae86e9138680aa0881
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-01-03 00:19:35 -0500

    hi

--- src/server.ts
+++ src/server.ts
@@ -7,9 +7,11 @@ app.listen(port, async () => {
   console.log(`[Server] Running on port ${port}`);
   try {
     const test = await prisma.mediaWikiRecentChange.findFirst();
+    await prisma.mediaWikiRecentChange.deleteMany();
     console.log("[Server] Database connection successful");
   } catch (error) {
     console.log("[Server] Database connection failed");
+    console.error(error);
     process.exit(1);
   }
 });
