7b32e85b2e8b4457f513bf6f1cd0cbdc4f3f6f1b
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-01-09 17:39:26 -0500

    Push changes

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ concurrently \
   --names "web,server,engine,prisma" \
   "cd web && npm run dev" \
   "cd src && make" \
-  "cd src && npx prisma studio" \
+  "cd src && npx prisma studio --browser none" \
   "sleep 3 && cd engine && go run ./cmd"
