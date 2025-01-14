2e669137059663bf4dda5717142bd15446e08d9d
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-01-13 23:40:28 -0500

    remove start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-concurrently \
-  --names "web,server,engine,prisma" \
-  "cd web && npm run dev" \
-  "cd src && make" \
-  "cd src && npx prisma studio --browser none" \
-  "sleep 3 && cd engine && go run ./cmd"
