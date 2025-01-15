d0a1691c914f26806d4b180af6d6a3285f76b2c4
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2025-01-14 21:59:58 -0500

    start pr

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,8 +2,7 @@ import ActiveSpikes from "./components/spikes";
 
 export default function Home() {
   return (
-    <div className="p-4">
-      <h1 className="font-bold">pulse</h1>
+    <div className="justify-center flex">
       <ActiveSpikes />
     </div>
   );
